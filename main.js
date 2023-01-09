979d00edfaca6a9186654fa3189408aa23eaa419
arnemergan <arne.mergan@hotmail.com>
2023-01-09 22:14:44 +0100

    bump

--- main.js
+++ main.js
@@ -14,3 +14,11 @@ window.addEventListener("scroll", (e) => {
     navbar.classList.remove("sticky");
   }
 });
+
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll("nav a").forEach((link) => {
+  if (link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+    console.log(link);
+  }
+});
